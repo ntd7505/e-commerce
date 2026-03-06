6f10757347217fe4ba385b091b96ce9fc858338d
ndat7505 <nguyendat2knam1@gmail.com>
2026-03-06 18:20:26 +0700

    feat: implement user crud and soft delete

--- backend/src/main/java/com/NguyenDat/ecommerce/service/UserService.java
+++ backend/src/main/java/com/NguyenDat/ecommerce/service/UserService.java
@@ -1,15 +1,26 @@
 package com.NguyenDat.ecommerce.service;
 
-import com.NguyenDat.ecommerce.dto.response.StaffCreationResponse;
-import com.NguyenDat.ecommerce.mapper.UserMapper;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.NguyenDat.ecommerce.dto.request.UserCreationRequest;
+import com.NguyenDat.ecommerce.dto.request.UserUpdateRequest;
+import com.NguyenDat.ecommerce.dto.response.UserResponse;
+import com.NguyenDat.ecommerce.entity.Role;
 import com.NguyenDat.ecommerce.entity.User;
+import com.NguyenDat.ecommerce.exception.AppException;
+import com.NguyenDat.ecommerce.exception.ErrorCode;
+import com.NguyenDat.ecommerce.mapper.UserMapper;
+import com.NguyenDat.ecommerce.repository.RoleRepository;
 import com.NguyenDat.ecommerce.repository.UserRepository;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,21 +28,58 @@ import java.util.List;
 public class UserService {
     UserRepository userRepository;
 
+    RoleRepository roleRepository;
+
     UserMapper userMapper;
 
-    public void handleSaveUser(StaffCreationResponse staffCreationResponse) {
-
-
-//        this.userRepository.save(userMapper.toStaffCreationResponse())
+    public UserResponse createNewUsers(UserCreationRequest userCreationRequest) {
+        User user = userMapper.toUser(userCreationRequest);
+        if (userCreationRequest.getRoles() == null) {
+            Role role = roleRepository.findById("USER").orElseThrow(() -> new AppException(ErrorCode.ROLE_NOT_FOUND));
+            user.setRoles(Set.of(role));
+        } else {
+            Set<Role> roles = userCreationRequest.getRoles().stream()
+                    .map(roleName ->
+                            roleRepository.findById(roleName).orElseThrow(() -> new AppException(ErrorCode.ROLE_NOT_FOUND)))
+                    .collect(Collectors.toSet());
+            user.setRoles(roles);
+        }
+        try {
+            user = userRepository.save(user);
+        } catch (DataIntegrityViolationException ex) {
+            throw new AppException(ErrorCode.USER_EXISTED);
+        }
+        return userMapper.toStaffResponse(user);
     }
 
-    public StaffCreationResponse getUserById(long id) {
+    public UserResponse getUserById(long id) {
         User user = userRepository.getUserById(id);
-        return userMapper.toStaffCreationResponse(user);
+        if (user == null) {
+            throw new AppException(ErrorCode.USER_NOT_EXISTED);
+        }
+        return userMapper.toStaffResponse(user);
     }
 
-    public List<StaffCreationResponse> getAllUsers() {
-        return userRepository.findAll().stream().map(userMapper::toStaffCreationResponse).toList();
+    public List<UserResponse> getAllUsers() {
+        return userRepository.findAll().stream()
+                .map(userMapper::toStaffResponse)
+                .toList();
     }
 
+    public UserResponse updateStaffById(long id, UserUpdateRequest userUpdateRequest) {
+        User user = this.userRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+        userMapper.updateUserMapper(user, userUpdateRequest);
+        //        var role = roleRepository.findAllById(userUpdateRequest.getRoles());
+        //        user.setRoles(new HashSet<>(role));
+        return userMapper.toStaffResponse(userRepository.save(user));
+    }
+
+    public void deleteStaff(long id) {
+        User user = this.userRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+        if (user.isDeleted()) {
+            throw new AppException(ErrorCode.USER_NOT_EXISTED);
+        }
+        user.setDeleted(true);
+        this.userRepository.save(user);
+    }
 }
